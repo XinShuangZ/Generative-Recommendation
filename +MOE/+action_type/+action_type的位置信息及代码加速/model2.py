7c38d2758967bd8721dd31266a455008dad69b86
XinShuang-Zhang <Zhangxs@smail.nju.edu.cn>
2025-08-24 00:59:33 +0800

    Update model2.py

--- +MOE/+action_type/+action_type的位置信息及代码加速/model2.py
+++ +MOE/+action_type/+action_type的位置信息及代码加速/model2.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from tqdm import tqdm
+from dataset import save_emb
 
 def my_activation(method='relu'): 
     activation_map = {
