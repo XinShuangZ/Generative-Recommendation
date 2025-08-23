7f1a6e3b80c96c9a44ce31ba1f6213ef17e57d3b
XinShuang-Zhang <Zhangxs@smail.nju.edu.cn>
2025-08-23 22:36:26 +0800

    Update dataset.py

--- +MOE/+action_type/+action_type的位置信息及代码加速/dataset.py
+++ +MOE/+action_type/+action_type的位置信息及代码加速/dataset.py
@@ -351,3 +351,123 @@ class MyDataset(torch.utils.data.Dataset):
                 batch_data[i] = seq_data
 
             return torch.from_numpy(batch_data)
+
+
+
+class MyTestDataset(MyDataset):
+    """
+    测试数据集
+    """
+
+    def __init__(self, data_dir, args):
+        super().__init__(data_dir, args)
+
+    def _load_data_and_offsets(self):
+        self.data_file = open(self.data_dir / "predict_seq.jsonl", 'rb')
+        with open(Path(self.data_dir, 'predict_seq_offsets.pkl'), 'rb') as f:
+            self.seq_offsets = pickle.load(f)
+
+    def _process_cold_start_feat(self, feat):
+        """
+        处理冷启动特征。训练集未出现过的特征value为字符串，默认转换为0.可设计替换为更好的方法。
+        """
+        processed_feat = {}
+        for feat_id, feat_value in feat.items():
+            if type(feat_value) == list:
+                value_list = []
+                for v in feat_value:
+                    if type(v) == str:
+                        value_list.append(0)
+                    else:
+                        value_list.append(v)
+                processed_feat[feat_id] = value_list
+            elif type(feat_value) == str:
+                processed_feat[feat_id] = 0
+            else:
+                processed_feat[feat_id] = feat_value
+        return processed_feat
+
+    def __getitem__(self, uid):
+        """
+        获取单个用户的数据，并进行padding处理，生成模型需要的数据格式
+
+        Args:
+            uid: 用户在self.data_file中储存的行号
+        Returns:
+            seq: 用户序列ID
+            token_type: 用户序列类型，1表示item，2表示user
+            seq_feat: 用户序列特征，每个元素为字典，key为特征ID，value为特征值
+            user_id: user_id eg. user_xxxxxx ,便于后面对照答案
+        """
+        user_sequence = self._load_user_data(uid)  # 动态加载用户数据
+
+        ext_user_sequence = []
+        for record_tuple in user_sequence:
+            u, i, user_feat, item_feat, _, _ = record_tuple
+            if u:
+                if type(u) == str:  # 如果是字符串，说明是user_id
+                    user_id = u
+                else:  # 如果是int，说明是re_id
+                    user_id = self.indexer_u_rev[u]
+            if u and user_feat:
+                if type(u) == str:
+                    u = 0
+                if user_feat:
+                    user_feat = self._process_cold_start_feat(user_feat)
+                ext_user_sequence.insert(0, (u, user_feat, 2))
+
+            if i and item_feat:
+                # 序列对于训练时没见过的item，不会直接赋0，而是保留creative_id，creative_id远大于训练时的itemnum
+                if i > self.itemnum:
+                    i = 0
+                if item_feat:
+                    item_feat = self._process_cold_start_feat(item_feat)
+                ext_user_sequence.append((i, item_feat, 1))
+
+        seq = np.zeros([self.maxlen + 1], dtype=np.int32)
+        token_type = np.zeros([self.maxlen + 1], dtype=np.int32)
+        seq_feat = np.empty([self.maxlen + 1], dtype=object)
+
+        idx = self.maxlen
+        for record_tuple in reversed(ext_user_sequence[:-1]):
+            i, feat, type_ = record_tuple
+            feat = self.fill_missing_feat(feat, i)
+            seq[idx] = i
+            token_type[idx] = type_
+            seq_feat[idx] = feat
+            idx -= 1
+            if idx == -1:
+                break
+
+        seq_feat = np.where(seq_feat == None, self.feature_default_value, seq_feat)
+
+        return seq, token_type, seq_feat, user_id
+
+    def __len__(self):
+        """
+        Returns:
+            len(self.seq_offsets): 用户数量
+        """
+        with open(Path(self.data_dir, 'predict_seq_offsets.pkl'), 'rb') as f:
+            temp = pickle.load(f)
+        return len(temp)
+
+    def collate_fn(batch):
+        """
+        将多个__getitem__返回的数据拼接成一个batch
+
+        Args:
+            batch: 多个__getitem__返回的数据
+
+        Returns:
+            seq: 用户序列ID, torch.Tensor形式
+            token_type: 用户序列类型, torch.Tensor形式
+            seq_feat: 用户序列特征, list形式
+            user_id: user_id, str
+        """
+        seq, token_type, seq_feat, user_id = zip(*batch)
+        seq = torch.from_numpy(np.array(seq))
+        token_type = torch.from_numpy(np.array(token_type))
+        seq_feat = self.feat2tensor_all(list(seq_feat), include_user=True)
+        
+        return seq, token_type, seq_feat, user_id
